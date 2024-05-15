ec35f34bda73fb40034548c7303f96457a3bed0c
Jorge Vega Sánchez <jorvesan96@gmail.com>
2024-05-15 10:04:01 +0100

    Eliminacion libreria hdfs v4.0

--- datosCrossref.py
+++ datosCrossref.py
@@ -1,6 +1,5 @@
 from habanero import Crossref
 from datetime import datetime, timezone
-from hdfs import InsecureClient
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType
 import re
@@ -11,8 +10,6 @@ HDFS_USER = 'eii'
 HDFS_PATH_PROCESSED = '/datos/processed_crossref_data'
 LAST_UPDATE_PATH = '/datos/last_update.txt'
 
-# Inicializar cliente HDFS
-hdfs_client = InsecureClient(HDFS_URL, user=HDFS_USER)
 
 
 def extract_paragraphs(text):
@@ -23,16 +20,16 @@ def extract_paragraphs(text):
 # Función para obtener la fecha de la última actualización en formato timestamp
 def obtener_fecha_ultima_actualizacion():
     try:
-        with hdfs_client.read(LAST_UPDATE_PATH) as reader:
-            fecha_str = reader.read().decode('utf-8').strip()
+        with open(LAST_UPDATE_PATH, 'r') as file:
+            fecha_str = file.read().strip()
             return int(datetime.strptime(fecha_str, '%Y-%m-%d').replace(tzinfo=timezone.utc).timestamp())
-    except:
-        return 946684800  # Fecha inicial por defecto (1 de enero de 2000 en timestamp)
+    except FileNotFoundError:
+        return 946684800   # Fecha inicial por defecto (1 de enero de 2000 en timestamp)
 
 # Función para guardar la fecha de la última actualización en un archivo
 def guardar_fecha_ultima_actualizacion(fecha):
-    with hdfs_client.write(LAST_UPDATE_PATH, encoding='utf-8') as writer:
-        writer.write(fecha)
+    with open(LAST_UPDATE_PATH, 'w') as file:
+        file.write(fecha)
 
 # Función principal para descargar nuevos registros y procesarlos con Spark
 def descargar_y_procesar_datos():
